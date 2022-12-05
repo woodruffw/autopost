a6aa2beee69ad8783967fbe50791d5994042ba4f
William Woodruff <william@yossarian.net>
2022-12-04 23:11:31 -0800

    _cli: remove jekyll support for now
    This was going to be very annoying with Jekyll's URL flexibility,
    and Atom support handles my use case just as well.
    
    Signed-off-by: William Woodruff <william@yossarian.net>

--- autopost/_cli.py
+++ autopost/_cli.py
@@ -7,7 +7,6 @@ from pathlib import Path
 from typing import Iterator
 
 import feedparser
-import frontmatter
 import tomllib
 from rich import traceback
 from rich.console import Console
@@ -81,20 +80,6 @@ def _parser() -> argparse.ArgumentParser:
         type=str,
     )
 
-    jekyll = subcommands.add_parser(
-        "jekyll",
-        help="auto-post for a Jekyll-based blog",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    jekyll.add_argument(
-        "path",
-        metavar="PATH",
-        help="the blog's root directory",
-        type=Path,
-        default=_HERE,
-        nargs="?",
-    )
-
     return parser
 
 
@@ -102,16 +87,6 @@ def _parser() -> argparse.ArgumentParser:
 def _get_post(args: argparse.Namespace) -> Iterator[tuple[str, str, list[str]]]:
     if args.subcommand == "manual":
         yield args.content, args.url, args.tags
-    elif args.subcommand == "jekyll":
-        if not (args.path / "_config.yml").is_file() or not (args.path / "_posts").is_dir():
-            raise ValueError(f"{args.path} does not look like a Jekyll-based blog")
-
-        # Jekyll posts are always prefixed with the date, so the latest post is always
-        # the last one (or the first one, reverse-sorted).
-        latest_path = sorted((args.path / "_posts").glob("*.md"), reverse=True)[0].resolve()
-        post = frontmatter.loads(latest_path.read_text())
-
-        yield post["title"], None, []
     elif args.subcommand == "atom":
         feed = feedparser.parse(args.url)
         if not feed.entries:
